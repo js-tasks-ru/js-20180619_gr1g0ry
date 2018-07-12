f284f915275d338fe8145a2bb281df06bb75947a
Grigory <kir.grigory@gmail.com>
2018-07-12 22:25:23 +0300

    5_1

--- 5-module/1-task/index.js
+++ 5-module/1-task/index.js
@@ -5,4 +5,19 @@
  * @param {Element} table
  */
 function highlight(table) {
+    debugger
+    for (let i=1; i< table.rows.length; i+=1) {
+        let row = table.rows[i];
+        if (row.cells[3].dataset.available === undefined) {
+            row.setAttribute('hidden', true);
+        } else {
+            row.classList.add((row.cells[3].dataset.available === 'true') ? "available" : "unavailable");
+            row.classList.remove((row.cells[3].dataset.available === 'true') ?  "unavailable": "available");
+        }
+        row.classList.add(row.cells[2].innerText === 'm' ? 'male':'female');
+        if (Number(row.cells[1].innerText) < 18) {
+            row.style.cssText = "text-decoration: line-through";
+
+        }
+    }
 }
