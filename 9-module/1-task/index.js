b78de387edd314f36d12ccc1f504b00ea7fe121f
Burlak Ilia <burlak@psu.karelia.ru>
2018-07-31 21:28:46 +0300

    add solution for 9-module-1-task

--- 9-module/1-task/index.js
+++ 9-module/1-task/index.js
@@ -6,6 +6,8 @@
  * @param {Function} Parent - родительский класс
  */
 function extendClass(Main, Parent) {
-    // здесь ваш код наследования
+    // подробнее см здесь http://learn.javascript.ru/class-inheritance#%D0%BF%D0%BE%D0%BB%D0%BD%D1%8B%D0%B9-%D0%BA%D0%BE%D0%B4-%D0%BD%D0%B0%D1%81%D0%BB%D0%B5%D0%B4%D0%BE%D0%B2%D0%B0%D0%BD%D0%B8%D1%8F
+    Main.prototype = Object.create(Parent.prototype);
+    Main.prototype.constructor = Main;
 }
 
