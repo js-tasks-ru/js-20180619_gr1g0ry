75a05562659a3ad9d8cb911ba8c9ad4affea66be
Grigory <kir.grigory@gmail.com>
2018-08-01 22:11:40 +0300

    10_1

--- 10-module/1-task/index.js
+++ 10-module/1-task/index.js
@@ -21,6 +21,42 @@
         constructor(data) {
             this.el = document.createElement('table');
             this.data = data;
+
+            this.tableHead = document.createElement('thead');
+            this.tableBody = document.createElement('tbody');
+
+            this.tableHead.innerHTML = "" +
+                "<tr>" +
+                "  <td>Name</td>" +
+                "  <td>Age</td>" +
+                "  <td>Salary</td>" +
+                "  <td>City</td>" +
+                "  <td></td>" +
+                "</tr>";
+
+            this.data
+                .forEach(user => {
+                    let tr = document.createElement('tr');
+                    tr.dataset.userId = user.id;
+                    tr.innerHTML = `<td>
+                                        ${Object.values(user).slice(1).join('</td><td>')}
+                                    </td>
+                                    <td><a>X</a></td>`;
+                    this.tableBody.appendChild(tr);
+                });
+
+            this.el.appendChild(this.tableHead);
+            this.el.appendChild(this.tableBody);
+            this.el.addEventListener('click', e => {
+                if (e.target.outerHTML === '<a>X</a>') {
+
+                    let removingUserTr = e.target.closest('tr');
+                    let removingUserId = removingUserTr.dataset.userId;
+                    removingUserTr.parentElement.removeChild(removingUserTr);
+                    this.data = this.data.filter(user => user.id !== removingUserId);
+                    this.onRemoved(Number(removingUserId));
+                }
+            })
         }
 
         /**
